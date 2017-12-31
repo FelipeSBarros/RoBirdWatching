72ea85bcf75889cf204109563d33b55f65d789cd
Felipe Barros <felipe.b4rros@gmail.com>
2017-12-31 05:02:50 +0000

    #100DaysOfCode Now I added a function to confirm if there is a new doodle to download, comparing with the last saved (if any) in the database

--- Download.py
+++ Download.py
@@ -4,6 +4,7 @@
 import requests, os, bs4
 import sqlite3 as lite
 import sys
+from compareFunction import DoodleTest
 
 url = 'https://www.google.com/doodles'              # starting url
 os.makedirs('Doodles', exist_ok=True)   # store doodles in ./Doodles
@@ -34,32 +35,33 @@ else:
         prevLink = soup.select('a[rel="prev"]')[0]
         url = 'https://www.google.com/doodles' + prevLink.get('href')
 
+testResult = DoodleTest(comicAlt)
+if testResult is None or testResult is True:
     # Save the image to ./xkcd.
-imageFile = open(os.path.join('Doodles', os.path.basename(comicUrl)), 'wb')
-path = str(os.path.join('Doodles', os.path.basename(comicUrl)))
-for chunk in res.iter_content(100000):
-    imageFile.write(chunk)
+    imageFile = open(os.path.join('Doodles', os.path.basename(comicUrl)), 'wb')
+    path = str(os.path.join('Doodles', os.path.basename(comicUrl)))
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
 
-imageFile.close()
+    imageFile.close()
 
-print('Image saved; Saving data to SQLite')
+    print('Image saved; Saving data to SQLite')
 
-values = (comicDate,
-#.encode('ascii', 'ignore'), 
-comicAlt,#.encode('ascii', 'ignore'), 
-path)
+    values = (comicDate,
+              #.encode('ascii', 'ignore'), 
+              comicAlt,#.encode('ascii', 'ignore'), 
+    path)
 
-try:
-    con = lite.connect('mydb.db')
-    cur = con.cursor()
-    cur.execute("INSERT INTO Metadata(Date, Legend, Path) VALUES(?,?,?)",values)
-    con.commit()
-except lite.Error as e:
-    if con:
-        con.rollback()
-    print("Error %s:" % e.args[0])
-    sys.exit(1)
-finally:
-    if con:
-        con.close() 
-
+    try:
+        con = lite.connect('mydb.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO Metadata(Date, Legend, Path) VALUES(?,?,?)",values)
+        con.commit()
+    except lite.Error as e:
+        if con:
+            con.rollback()
+            print("Error %s:" % e.args[0])
+            sys.exit(1)
+    finally:
+        if con:
+            con.close() 
